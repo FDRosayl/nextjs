e9b1157ff7b707c06dc0b567fe11d1b03ffe21e7
FDRosayl <142879849+FDRosayl@users.noreply.github.com>
2023-11-24 11:35:57 -0800

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,10 +1,18 @@
 import React from "react"
 import Link from "next/link"
-export default function Cource() {
-    return (
-        <body><h1>Cource</h1>
-        <Link href={"/"}><h2>Home Page</h2></Link>
-        <button></button>
+ function Home(){
+  return (
+   <body>
+    <h1>Home</h1>
+<Link href={"/about"}><h2>about</h2></Link>
+<button ><h3>blog</h3></button>
+<h4>contact</h4>
+<h5>cource</h5>
 </body>
-    )
-}
+   
+   
+      
+  
+  )
+  }
+  export default Home
